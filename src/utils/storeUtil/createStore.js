07bb01a84fd75483cf01fcf8975d1440f1bf7d46
shaozhiyuan <yuanhjty@163.com>
2020-01-13 18:40:04 +0800

    docs: createStore() examples

--- src/utils/storeUtil/createStore.js
+++ src/utils/storeUtil/createStore.js
@@ -6,32 +6,6 @@ import injectReducers from './injectReducers';
 
 const { isArray } = Array;
 
-/**
- * @param config
- * @example
- *   createStore({
- *     name: 'user', // required
- *     initialState: defaultInitialState,
- *     actions: {
- *       set: () => ({ }),
- *       reset: () => ({ })
- *     },
- *     reducers: {
- *       set: (state, action) => nextState,
- *       reset: (state, action) => nextState,
- *     },
- *   })
- *   createStore({
- *     name: 'user',
- *     slices: [
- *       { name: 's1', initialState, actions: { set, reset }, reducers: { set, reset } },
- *       { name: 's2', slices: [{ name: 'ss1', initialState, actions: { set, reset }, reducers: { set, reset } }] },
- *     ],
- *   }) => 返回 [{
- *     s1: { set, reset },
- *     s2: { ss1: { set, reset } },
- *   }, reducer]
- */
 function _createStore(config) {
   const safeConfig = config || {};
 
@@ -97,6 +71,31 @@ function _createStore(config) {
   return null;
 }
 
+/**
+ * @example
+ *   createStore({
+ *     name: 'user', // required
+ *     initialState: defaultInitialState,
+ *     actions: {
+ *       set: () => ({ }),
+ *       reset: () => ({ })
+ *     },
+ *     reducers: {
+ *       set: (state, action) => nextState,
+ *       reset: (state, action) => nextState,
+ *     },
+ *   })
+ *   createStore({
+ *     name: 'user',
+ *     slices: [
+ *       { name: 's1', initialState, actions: { set, reset }, reducers: { set, reset } },
+ *       { name: 's2', slices: [{ name: 'ss1', initialState, actions: { set, reset }, reducers: { set, reset } }] },
+ *     ],
+ *   }) => 返回 [{
+ *     s1: { set, reset },
+ *     s2: { ss1: { set, reset } },
+ *   }, reducer]
+ */
 export default function createStore(config) {
   const store = _createStore(config);
   if (store) {
